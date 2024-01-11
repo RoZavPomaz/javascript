58da0e86f530b392c3de956fba613bbee56bf435
Roberto Zavala <robertozavala666@gmail.com>
2024-01-11 01:30:33 -0500

    ejemplos Js

---
+++ app.js
@@ -0,0 +1,70 @@
+// Variables
+let edad = 20;
+let hijos = 10;
+let numeromayor = 50;
+let numeromenor = 10;
+let lista = 30
+const year = 2024;
+const minombre = "Roberto"
+
+// Tipos de datos:
+
+// Number, string, boolean
+// Number
+let numero = 123;
+
+// String
+let nombre = "roberto";
+
+// Boolean
+var academy = false;
+
+// numerico (entero y flotante/double)
+//  Entero
+let numerohijos = 5;
+
+// Flotante
+let vuelto = 5.4;
+
+// Operadores aritmeticos
+console.log(numeromayor - numeromenor);
+console.log(numeromayor + numeromenor);
+console.log(numeromayor / numeromenor);
+console.log(numeromayor * numeromenor);
+
+// Modulo
+console.log(numeromayor % numeromenor);
+
+
+// Operadores de asignacion
+
+let lugar = "mexico";
+
+lugar = "miami";
+
+// Operadores de comparacion
+
+console.log(numeromayor < numeromenor)
+
+console.log(numeromayor > numeromenor)
+
+console.log(numeromayor >= 100);
+
+console.log(numeromayor <= numeromenor);
+
+
+// Operadores logicos
+
+// and
+const edadenlista = edad >= lista && hijos == 10;
+
+console.log(edadenlista)
+
+// or
+
+const hijosedad = edad >= lista || hijos == 10;
+
+console.log(hijosedad)
+
+
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+TAMBIEN LEA MI <<<<<APP.JS>>>>>
+
+
+1.Programación:
+Programar es crear un conjunto de instrucciones para que una computadora realice una tarea específica. Implica decirle a la máquina qué hacer y cómo hacerlo.
+
+2.Variables:
+Las variables son espacios de almacenamiento que tienen nombres y pueden guardar información. Son como cajas donde puedes poner valores, y esos valores pueden cambiar mientras el programa se ejecuta.
+
+3.Tipos de Datos:
+Los tipos de datos son categorías que definen qué tipo de información puede ser almacenada. Pueden ser números, palabras, listas de cosas, etc. Ayudan a la computadora a entender y manejar diferentes tipos de información.
+
+4.Operadores:
+Los operadores son herramientas que realizan acciones en la programación. Pueden sumar números, comparar cosas, o asignar valores a variables. Son como las herramientas que usas para construir o cambiar cosas en tu programa.
